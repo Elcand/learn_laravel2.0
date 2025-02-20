ec4c2954080149c6a18352a70dcdf63431b73705
Elcand <tjandraraharja99@gmail.com>
2025-02-20 11:03:43 +0700

    belajar SingelActionController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SingleActionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/about', [HomeController::class, 'about']);
+
+Route::get('/single-action', SingleActionController::class);
 
 // Route::get('/contact', function () {
 //     $title = 'Contact!';
