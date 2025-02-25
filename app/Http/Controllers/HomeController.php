e7266d34400eaa473d89ab7e51372ac52b15ae3f
Elcand <tjandraraharja99@gmail.com>
2025-02-25 13:14:32 +0700

    belajar query builder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,34 +10,9 @@ class HomeController extends Controller
 {
     public function index()
     {
-        // // insert a new data
-        // DB::table('users')->insert([
-        //     [
-        //         'name' => 'Moses',
-        //         'email' => 'mosses@gmail.com',
-        //         'password' => bcrypt('password'),
-        //     ],
-        //     [
-        //         'name' => 'Lewandowski',
-        //         'email' => 'lewaan@gmail.com',
-        //         'password' => bcrypt('password'),
-        //     ]
-        // ]);
 
-        // //get data from db
-        // $users =  DB::table('users')->where('id', '>', 3)->get();
-        // return $users;
-
-        // //update data
-        // DB::table('users')->where('id', 9)->update([
-        //     'name' => 'Messsssssssuuuuuuiiiiiiiiii',
-        //     'email' => 'suiiiiii@gmail.com.updated',
-        //     'password' => bcrypt('password'),
-        // ]);
-
-        // //delete from db
-        // DB::table('users')->where('id', '>', 8)->delete();
-
+        $products = DB::table('products')->avg('price');
+        dd($products);
         return view('home');
     }
 
