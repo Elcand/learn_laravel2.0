c412c6e047e49cd6578b8e5eaaf5f5a98e626f00
Elcand <tjandraraharja99@gmail.com>
2025-02-25 14:47:33 +0700

    belajar Eloquent - membuat data baru

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -10,9 +12,18 @@ class HomeController extends Controller
 {
     public function index()
     {
+        // //create data
+        // $user = new User(); // contoh 1
+        // $user->name = 'Coki pardede';
+        // $user->email = 'coki.pardede@gmail.com';
+        // $user->password = 'password';
+        // $user->save();
 
-        $products = DB::table('products')->avg('price');
-        dd($products);
+        // $products = new Product(); // contoh 2
+        // $products->name = 'Make Time';
+        // $products->description = 'Buku';
+        // $products->price = '75000';
+        // $products->save();
         return view('home');
     }
 
